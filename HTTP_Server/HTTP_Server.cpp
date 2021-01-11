d712f86b47f3c2da4b7683b8ef6bfb428111eccf
AnnaKhalikova <hanna.Khalikova@gmail.com>
2021-01-12 00:45:43 +0300

    Add form loading and connections to the project

--- HTTP_Server/HTTP_Server.cpp
+++ HTTP_Server/HTTP_Server.cpp
@@ -1,72 +1,77 @@
 #include <iostream>
 #include <sstream>
 #include <string>
-#include<sdkddkver.h>
-#include<conio.h>
-#include<stdio.h>
-#include<iostream>
+#include <fstream>
+#include <sdkddkver.h>
+#include <conio.h>
+#include <stdio.h>
+#include <iostream>
 
-// Для корректной работы freeaddrinfo в MinGW
-// Подробнее: http://stackoverflow.com/a/20306451
+ //Для корректной работы freeaddrinfo в MinGW
+ //Подробнее: http://stackoverflow.com/a/20306451
 #define _WIN32_WINNT 0x501
+
 
 #include <WinSock2.h>
 #include <WS2tcpip.h>
 
-// Необходимо, чтобы линковка происходила с DLL-библиотекой
-// Для работы с сокетам
+ //Необходимо, чтобы линковка происходила с DLL-библиотекой
+ //Для работы с сокетам
 #pragma comment(lib, "Ws2_32.lib")
-
+using namespace std;
 using std::cerr;
 
-int calculatePercent(int actual);
+int calculatePercent(char buffer[]);
+
 
 int main()
 {
+     
+
     WSADATA wsaData; // служебная структура для хранение информации
-    // о реализации Windows Sockets
-    // старт использования библиотеки сокетов процессом
-    // (подгружается Ws2_32.dll)
+    /* о реализации Windows Sockets
+     старт использования библиотеки сокетов процессом
+     (подгружается Ws2_32.dll)*/
     int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-    // Если произошла ошибка подгрузки библиотеки
+     /*Если произошла ошибка подгрузки библиотеки*/
     if (result != 0) {
         cerr << "WSAStartup failed: " << result << "\n";
         return result;
     }
 
     struct addrinfo* addr = NULL; // структура, хранящая информацию
-    // об IP-адресе  слущающего сокета
+     /*об IP-адресе  слущающего сокета
 
-    // Шаблон для инициализации структуры адреса
+     Шаблон для инициализации структуры адреса*/
     struct addrinfo hints;
     ZeroMemory(&hints, sizeof(hints));
 
     hints.ai_family = AF_INET; // AF_INET определяет, что будет
-    // использоваться сеть для работы с сокетом
+     //использоваться сеть для работы с сокетом
     hints.ai_socktype = SOCK_STREAM; // Задаем потоковый тип сокета
     hints.ai_protocol = IPPROTO_TCP; // Используем протокол TCP
     hints.ai_flags = AI_PASSIVE; // Сокет будет биндиться на адрес,
-    // чтобы принимать входящие соединения
+     /*чтобы принимать входящие соединения
 
-    // Инициализируем структуру, хранящую адрес сокета - addr
-    // Наш HTTP-сервер будет висеть на 8000-м порту локалхоста
+     Инициализируем структуру, хранящую адрес сокета - addr
+     Наш HTTP-сервер будет висеть на 8000-м порту локалхоста*/
     result = getaddrinfo("127.0.0.1", "8000", &hints, &addr);
 
-    // Если инициализация структуры адреса завершилась с ошибкой,
-    // выведем сообщением об этом и завершим выполнение программы
+     /*Если инициализация структуры адреса завершилась с ошибкой,
+     выведем сообщением об этом и завершим выполнение программы*/
     if (result != 0) {
         cerr << "getaddrinfo failed: " << result << "\n";
         WSACleanup(); // выгрузка библиотеки Ws2_32.dll
         return 1;
     }
 
-    // Создание сокета
+     //Создание сокета
     int listen_socket = socket(addr->ai_family, addr->ai_socktype,
         addr->ai_protocol);
-    // Если создание сокета завершилось с ошибкой, выводим сообщение,
-    // освобождаем память, выделенную под структуру addr,
-    // выгружаем dll-библиотеку и закрываем программу
+     /*Если создание сокета завершилось с ошибкой, выводим сообщение,
+     освобождаем память, выделенную под структуру addr,
+     выгружаем dll-библиотеку и закрываем программу*/
     if (listen_socket == INVALID_SOCKET) {
         cerr << "Error at socket: " << WSAGetLastError() << "\n";
         freeaddrinfo(addr);
@@ -74,13 +79,13 @@ int main()
         return 1;
     }
 
-    // Привязываем сокет к IP-адресу
+    /* Привязываем сокет к IP-адресу*/
     result = bind(listen_socket, addr->ai_addr, (int)addr->ai_addrlen);
 
-    // Если привязать адрес к сокету не удалось, то выводим сообщение
-    // об ошибке, освобождаем память, выделенную под структуру addr.
-    // и закрываем открытый сокет.
-    // Выгружаем DLL-библиотеку из памяти и закрываем программу.
+     /*Если привязать адрес к сокету не удалось, то выводим сообщение
+     об ошибке, освобождаем память, выделенную под структуру addr.
+     и закрываем открытый сокет.
+     Выгружаем DLL-библиотеку из памяти и закрываем программу.*/
     if (result == SOCKET_ERROR) {
         cerr << "bind failed with error: " << WSAGetLastError() << "\n";
         freeaddrinfo(addr);
@@ -89,7 +94,7 @@ int main()
         return 1;
     }
 
-    // Инициализируем слушающий сокет
+     //Инициализируем слушающий сокет
     if (listen(listen_socket, SOMAXCONN) == SOCKET_ERROR) {
         cerr << "listen failed with error: " << WSAGetLastError() << "\n";
         closesocket(listen_socket);
@@ -105,30 +110,7 @@ int main()
     for (;;) {
         std::stringstream response; // сюда будет записываться ответ клиенту
         std::stringstream response_body; // тело ответа
-        //ТЕСТИРУЕМ
-        buf[result] = '\0';
-
-        // Данные успешно получены
-        // формируем тело ответа (HTML)
-        response_body << "<title>Тег FORM, атрибут method</title>\n"
-            << "<form method= " << "\"get\"" << ">\n"
-            << "<input type=" << "\"hidden\"" << "name=" << "\"test\"" << "value=" << "\"123\"" << ">\n"
-            << "<input type=" << "\"submit\"" << "value=" << "\"Отправить\"" << ">\n"
-            << "</form>";
-
-        // Формируем весь ответ вместе с заголовками
-        response << "HTTP/1.1 200 OK\r\n"
-            << "Version: HTTP/1.1\r\n"
-            << "Content-Type: text/html; charset=utf-8\r\n"
-            << "Content-Length: " << response_body.str().length()
-            << "\r\n\r\n"
-            << response_body.str();
-
-        // Отправляем ответ клиенту с помощью функции send
-        result = send(client_socket, response.str().c_str(),
-            response.str().length(), 0);
-        //ТЕСТИРУЕМ
-        // Принимаем входящие соединения
+         //Принимаем входящие соединения
         client_socket = accept(listen_socket, NULL, NULL);
         if (client_socket == INVALID_SOCKET) {
             cerr << "accept failed: " << WSAGetLastError() << "\n";
@@ -142,29 +124,43 @@ int main()
 
 
         if (result == SOCKET_ERROR) {
-            // ошибка получения данных
+             //ошибка получения данных
             cerr << "recv failed: " << result << "\n";
             closesocket(client_socket);
         }
         else if (result == 0) {
-            // соединение закрыто клиентом
+             //соединение закрыто клиентом
             cerr << "connection closed...\n";
         }
         else if (result > 0) {
-            // Мы знаем фактический размер полученных данных, поэтому ставим метку конца строки
-            // В буфере запроса.
+            /* Мы знаем фактический размер полученных данных, поэтому ставим метку конца строки
+             В буфере запроса.*/
+            //char indexPage[max_client_buffer_size];
+            std::ifstream in("C:\\Users\\aikha\\Downloads\\HTTP_Server\\Debug\\index.html"); // окрываем файл для чтения
+
+
+            string indexPage = "", s1;
+            while (getline(in, s1))
+                indexPage += s1;
+  
+            in.close();
+
             buf[result] = '\0';
 
-            // Данные успешно получены
-            // формируем тело ответа (HTML)
-            response_body << "<title>Test C++ HTTP Server</title>\n"
+             /*Данные успешно получены
+             формируем тело ответа (HTML)*/
+             response_body << indexPage << "\n" << calculatePercent(buf);
+             //TEMP COMMAND FOR TEST
+             //std::cout << indexPage;
+            
+           /* response_body << "<title>Test C++ HTTP Server</title>\n"
                 << "<h1>Test page</h1>\n"
-                << "<p>This is body of the test page...</p>\n"
+                << "<input>This is body of the test page...</p>\n"
                 << "<h2>Calculate percents:</h2>\n"
-                << "<pre>" << calculatePercent(50) << "%</pre>\n"
-                << "<em><small>Test C++ Http Server</small></em>\n";
+                << "<pre>" << calculatePercent(buf) << "%</pre>\n"
+                << "<em><small>Test C++ Http Server</small></em>\n";*/
 
-            // Формируем весь ответ вместе с заголовками
+             /*Формируем весь ответ вместе с заголовками*/
             response << "HTTP/1.1 200 OK\r\n"
                 << "Version: HTTP/1.1\r\n"
                 << "Content-Type: text/html; charset=utf-8\r\n"
@@ -172,25 +168,31 @@ int main()
                 << "\r\n\r\n"
                 << response_body.str();
 
-            // Отправляем ответ клиенту с помощью функции send
+             //Отправляем ответ клиенту с помощью функции send
             result = send(client_socket, response.str().c_str(),
                 response.str().length(), 0);
 
             if (result == SOCKET_ERROR) {
-                // произошла ошибка при отправле данных
+                 //произошла ошибка при отправле данных
                 cerr << "send failed: " << WSAGetLastError() << "\n";
             }
-            // Закрываем соединение к клиентом
+             //Закрываем соединение к клиентом
             closesocket(client_socket);
         }
     }
 
-    // Убираем за собой
+     //Убираем за собой
     closesocket(listen_socket);
     freeaddrinfo(addr);
     WSACleanup();
     return 0;
 }
-int calculatePercent(int actual) {
-    return actual * 100 / 100;
+int calculatePercent(char buffer[]) {
+    cout << "Current info in buffer: " << buffer << endl;
+    int length = 0;
+
+    while (buffer[length] != '\0') {
+        length++;
+    }
+    return length;
 }
